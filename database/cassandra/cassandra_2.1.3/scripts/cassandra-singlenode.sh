d8e1f9df3eeec26c0098a88a53358f1002320db8
Daniel Malon <daniel.malon@me.com>
2015-02-21 09:48:57 +0000

    Fix cassandra 2.1.3 initialisation
    Cassandra 2.1 needs a specific broadcast address if the rpc_address is
    set to 0.0.0.0

--- database/cassandra/cassandra_2.1.3/scripts/cassandra-singlenode.sh
+++ database/cassandra/cassandra_2.1.3/scripts/cassandra-singlenode.sh
@@ -13,6 +13,9 @@ CASSANDRA_CONFIG=/opt/cassandra/conf
 # Dunno why zeroes here
 sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/" $CASSANDRA_CONFIG/cassandra.yaml
 
+# Set broadcast to containers IP
+sed -i -e "s/^# broadcast_rpc_address.*/broadcast_rpc_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
+
 # Be your own seed
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" $CASSANDRA_CONFIG/cassandra.yaml
 
